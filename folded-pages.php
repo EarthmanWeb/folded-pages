7640f748087a2db4f48f659780d4c5f5d63ab164
Terrance Orletsky <terran@earthman.ca>
2022-09-07 11:05:29 -0700

    Updates formatter path, version

--- folded-pages.php
+++ folded-pages.php
@@ -6,7 +6,7 @@
  * Author:          Earthman Media - Terrance Orletsky
  * Author URI:      earthmanmedia.com
  * Text Domain:     folded-pages
- * Version:         2.0.4
+ * Version:         2.0.5
  *
  * @package         Folded_Pages
  */
